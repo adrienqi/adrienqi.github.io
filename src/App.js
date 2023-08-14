c4ef9d2341d77d81c27a981635345d524b4e0e00
adriencheese <adrien.qi304@gmail.com>
2023-08-14 03:20:53 -0400

    sync

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
 import logo from './logo.svg';
+import nightcar from './imgs/nightcarfinal.jpg'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
+      <a
+        href="https://open.spotify.com/playlist/68dV6odIn9LOItqkGcb2Wi?si=bd253b1e958d43dd"
+      >
+        <button type="submit">
+          <img src={nightcar} alt={logo} className='nightcar' border='0'></img>
+        </button>
+      </a>
+      <div className="Intro">
+        ADRIEN QI
+        {/* <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
           Learn React
-        </a>
-      </header>
+        </a> */}
+      </div>
     </div>
   );
 }
